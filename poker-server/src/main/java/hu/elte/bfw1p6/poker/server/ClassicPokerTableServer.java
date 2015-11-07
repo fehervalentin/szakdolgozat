fa3c0b8cd1abf7a0d068b224a7915f2f096afb4e
fehervalentin@gmail.com <fehervalentin@gmail.com>
2015-11-07 23:27:51 +0100

    hand eval for 5 cards probably works...

--- poker-server/src/main/java/hu/elte/bfw1p6/poker/server/ClassicPokerTableServer.java
+++ poker-server/src/main/java/hu/elte/bfw1p6/poker/server/ClassicPokerTableServer.java
@@ -1,9 +1,12 @@
 package hu.elte.bfw1p6.poker.server;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.cantero.games.poker.texasholdem.Card;
+import com.cantero.games.poker.texasholdem.IPlayer;
 
 import hu.elte.bfw1p6.poker.client.observer.RemoteObserver;
 import hu.elte.bfw1p6.poker.command.HouseCommand;
@@ -11,6 +14,7 @@ import hu.elte.bfw1p6.poker.command.PlayerCommand;
 import hu.elte.bfw1p6.poker.command.classic.ClassicHouseCommand;
 import hu.elte.bfw1p6.poker.command.classic.ClassicPlayerCommand;
 import hu.elte.bfw1p6.poker.command.classic.type.ClassicHouseCommandType;
+import hu.elte.bfw1p6.poker.command.holdem.HoldemHouseCommand;
 import hu.elte.bfw1p6.poker.exception.PokerDataBaseException;
 import hu.elte.bfw1p6.poker.exception.PokerUserBalanceException;
 import hu.elte.bfw1p6.poker.model.entity.PokerPlayer;
@@ -152,7 +156,22 @@ public class ClassicPokerTableServer extends AbstractPokerTableServer {
 
 	@Override
 	protected void winner(HouseCommand houseCommand) {
-		// TODO Auto-generated method stub
-
+		ClassicHouseCommand holdemHouseCommand = (ClassicHouseCommand)houseCommand;
+		//TODO: aki foldolt annak ne vegyük figyelembe a lapjait lehet hogy már kész...
+		List<IPlayer> winner = HoldemHandEvaluator.getInstance().getWinner(new ArrayList<>(), players);
+		Card[] cards = winner.get(0).getCards();
+		// TODO: és mi van ha döntetlen?
+		int winner_ = -1;
+		System.out.println("Players size: " + players.size());
+		for (int i = 0; i < players.size(); i++) {
+			if (players.get(i).getCards()[0].equals(cards[0]) && players.get(i).getCards()[1].equals(cards[1])) {
+				//winner_ = (i - foldCounter) % playersInRound;
+				winner_ = i;
+				break;
+			}
+		}
+		System.out.println("A győztes sorszáma: " + winner_);
+		System.out.println("A győztes kártyalapjai: " + Arrays.toString(cards));
+		holdemHouseCommand.setUpWinnerCommand(cards, winner_);
 	}
 }
